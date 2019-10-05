e816a0ce5a5f57d5ec65b6cc90090f09ad401ed2
artmoiseev <artur.moiseev1@gmail.com>
2019-10-05 12:07:06 +0300

    Schedule page

--- my_otus_application/static/js/scripts.js
+++ my_otus_application/static/js/scripts.js
@@ -1,11 +1,30 @@
 function myFunction() {
-    var cards_section = document.getElementById("cards");
-    var button_show_hide = document.getElementById("show-hide-button")
+    let cards_section = document.getElementById("cards");
+    let button_show_hide = document.getElementById("show-hide-button");
     if (cards_section.style.display === "none") {
         cards_section.removeAttribute('style');
         button_show_hide.innerHTML = "Hide courses"
     } else {
         cards_section.style.display = "none";
         button_show_hide.innerHTML = "Show courses"
+    }
+}
+
+function filterTable() {
+    let input = document.getElementById("inputField");
+    let filter = input.value.toUpperCase();
+    let table = document.getElementById("myTable");
+    let tr = table.getElementsByTagName("tr");
+
+    for (let i = 0; i < tr.length; i++) {
+        let td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            let txtValue = td.innerHTML;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
     }
 }
